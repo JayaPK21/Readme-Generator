1b9f985df1720a95678237599957a0f4e5def86e
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-02-17 06:48:25 +0000

    Adds the questions structure to get user input for the application

--- index.js
+++ index.js
@@ -5,7 +5,54 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: 'What is your Project Title? ',
+        name: 'title'
+    },
 
+    {
+        type: 'list',
+        message: 'Select a License for your project from the following list: ',
+        name: 'license',
+        choices: ['MIT', 'Open Software License 3.0', 'Microsoft Public License', 'Academic Free License v3.0']
+    },
+
+    {
+        type: 'input',
+        message: 'Enter a description about your project: ',
+        name: 'description'
+    },
+
+    {
+        type: 'input',
+        message: 'Enter the Installation instructions for your application: ',
+        name: 'installation'
+    },
+
+    {
+        type: 'input',
+        message: 'Enter the Usage instructions for your application: ',
+        name: 'usage'
+    },
+
+    {
+        type: 'input',
+        message: 'How can others contribute towards this project? ',
+        name: 'contributing'
+    },
+
+    {
+        type: 'input',
+        message: 'Enter some test cases for your application: ',
+        name: 'tests'
+    },
+
+    {
+        type: 'input',
+        message: 'Enter your email id for users to contact you with questions: ',
+        name: 'emailID'
+    }
 ];
 
 // function to write README file
@@ -17,7 +64,8 @@ function init() {
 
     inquirer.prompt(questions).then((response) => {
 
-        writeToFile(fileName, generateMarkdown(response));
+        console.log(response.title, response.license);
+        //writeToFile(fileName, generateMarkdown(response));
         
     });
 
